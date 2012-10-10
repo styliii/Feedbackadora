afc32b17c5fee28bb10ce7e9791886b4464568fd
Li Ouyang <li.ouyang@gmail.com>
2012-10-10 14:21:12 -0400

    Feedbackadora

---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  instructor BOOLEAN,
+  name TEXT
+);
+
+CREATE TABLE issues (
+  id INTEGER PRIMARY KEY,
+  creator_id INTEGER,
+  content TEXT,
+  status_id INTEGER DEFAULT 1,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE statuses (
+  id INTEGER PRIMARY KEY,
+  content TEXT
+);
+
+CREATE TABLE solutions (
+  id INTEGER PRIMARY KEY,
+  issue_id INTEGER,
+  submitted_by INTEGER,
+  accepted BOOLEAN DEFAULT 0,
+  submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
