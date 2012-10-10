1651309303b64ea9d1a00a1b66dc16578eb84f7f
Li Ouyang <li.ouyang@gmail.com>
2012-10-10 14:44:41 -0400

    added new seed rows

--- seed.sql
+++ seed.sql
@@ -3,7 +3,8 @@ INSERT INTO users (name)
 VALUES
 ("Li"),
 ("Josh"),
-("Matt");
+("Matt"),
+("Aaron");
 
 -- create instructor
 INSERT INTO users (instructor, name)
@@ -23,9 +24,16 @@ VALUES
 -- create issues
 INSERT INTO issues (creator_id, title, content)
 VALUES
-(1, "can't insert image into octopress blog", "whatevs");
+(1, "can't insert image into octopress blog", "whatevs"),
+(2, "neighbor plays music too loud", "what do i do?"),
+(4, "github deleted all my files again", "please help me."),
+(3, "can't get my screen to turn on", "should i try restarting");
 
 -- submit solution
 INSERT INTO solutions (issue_id, submitted_by, content)
 VALUES
-(1, 2, "figure it out yourself, dummy.");
+(1, 5, "figure it out yourself, dummy.");
+(2, 3, "buy earphones"),
+(3, 1, "git rollback"),
+(2, 5, "call your landlord"),
+(4, 4, "yes, always");
